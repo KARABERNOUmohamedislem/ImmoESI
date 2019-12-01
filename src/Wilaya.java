a2050649fb48b0bc3e13e368c0cf89d4f3ab0cc4
Kara Bernou islam <44534706+KARABERNOUmohamedislem@users.noreply.github.com>
2019-12-01 22:00:24 +0100

    Add files via upload

---
+++ src/Wilaya.java
@@ -0,0 +1,11 @@
+public class Wilaya {
+    private String NomWilaya;
+    private int Prix_me;
+
+    public Wilaya(String NomWilaya,int prix)
+    {
+        this.NomWilaya=NomWilaya;
+        this.Prix_me=prix;
+    }
+
+}
